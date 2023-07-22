74d06d4b71743f903ec4f38488efc69e0e872119
KalidNadere <131591052+KalidNadere@users.noreply.github.com>
2023-07-22 16:29:13 +1000

    added questions to prompt user, code to create README file

--- index.js
+++ index.js
@@ -38,3 +38,69 @@ function generateREADME(answers) {
   - Github: [${answers.username}](https://github.com/${answers.username})
   `;
 }
+
+// Prompt the user with questions
+inquirer
+.prompt ([
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a project description:',
+  },
+  {
+    type: 'input',
+    name: 'instalaltion',
+    message: 'Enter installation instructions:',
+  },
+  { 
+    type: 'input',
+    name: 'usage',
+    message:'Provide usage information:',
+  },
+  {
+    type: 'list',
+    name: 'licence',
+    message: 'Choose  a licence for your application:',
+    choices: ['MIT', 'Apache', 'GPL', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter your Github username',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+])
+.then((answers) => {
+  const READMEContent = generatorREADME(answers);
+
+  // Create the README file
+  fs.writeFile('README.md', READMEContent, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('README.md successfully created!');
+  });
+})
+.catch((error) => {
+  console.error(error);
+});
